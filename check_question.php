08f62a6cd5195c719bdaacd28c4817aa8393a321
001 <001@mail>
2023-08-14 00:47:49 +0700

    fix

--- check_question.php
+++ check_question.php
@@ -12,11 +12,11 @@ $height = $_POST["height"];
 $weight = $_POST["weight"];
 $pressure = $_POST["pressure"];
 $pulse = $_POST["pulse"];
-$location = $_POST["location"];
-$period = $_POST["selected_hours"];
-$reason1 = $_POST["reason1"];
-$reason2 = $_POST["reason2"];
-$exer = $_POST["exer"];
+$location = $_POST["location"] ?? [];
+$period = $_POST["selected_hours"] ?? [];
+$reason1 = $_POST["reason1"] ?? [];
+$reason2 = $_POST["reason2"] ?? [];
+$exer = $_POST["exer"] ?? [];
 $pulseAfter = $_POST["pulseAfter"];
 $week = $_POST["week"];
 $duration = $_POST["duration"];
@@ -28,19 +28,19 @@ $community = $_POST["community"];
 $loc_agency = $_POST["loc_agency"];
 $business = $_POST["business"];
 
-// F5
+//F5
 $exper_sports = $_POST["exper_sports"];
 $res = $_POST["res"];
 $pub_res = $_POST["pub_res"];
 
-// F6
+//F6
 $train_exper_exer = $_POST["train_exper_exer"];
 $train_exper = $_POST["train_exper"];
 
-// F7
+//F7
 $vol_exper = $_POST["vol_exper"];
 
-// F8
+//F8
 $org_heal = $_POST["org_heal"];
 $pro_org_exer = $_POST["pro_org_exer"];
 $activity = $_POST["activity"];
@@ -68,110 +68,178 @@ $exerInput = $_POST["exerInput"];
 
 //echo ข้อมูล
 
-echo "user = ".$user .'<br>';
-echo "name = ".$name .'<br>';
-echo "surname = ".$surname .'<br>';
-echo "sex = ".$sex .'<br>'; 
-echo "province_id = ".$province_id .'<br>'; 
-echo "amphure_id = ".$amphure_id .'<br>'; 
-echo "age = ".$age .'<br>'; 
-echo "eduOptions = ".$eduOptions .'<br>'; 
-echo "eduInput = ".$eduInput .'<br>'; 
-echo "occOptions = ".$occOptions .'<br>'; 
-echo "occInput = ".$occInput .'<br>'; 
-echo "maryOptions = ".$maryOptions .'<br>'; 
-echo "maryInput = ".$maryInput .'<br>'; 
-echo "nationOptions = ".$nationOptions .'<br>'; 
-echo "nationInput = ".$nationInput .'<br>';
+echo "user = " . $user . '<br>';
+echo "name = " . $name . '<br>';
+echo "surname = " . $surname . '<br>';
+echo "sex = " . $sex . '<br>';
+echo "province_id = " . $province_id . '<br>';
+echo "amphure_id = " . $amphure_id . '<br>';
+echo "age = " . $age . '<br>';
+echo "eduOptions = " . $eduOptions . '<br>';
+echo "eduInput = " . $eduInput . '<br>';
+echo "occOptions = " . $occOptions . '<br>';
+echo "occInput = " . $occInput . '<br>';
+echo "maryOptions = " . $maryOptions . '<br>';
+echo "maryInput = " . $maryInput . '<br>';
+echo "nationOptions = " . $nationOptions . '<br>';
+echo "nationInput = " . $nationInput . '<br>';
 
-echo "height = ".$height .'<br>'; 
-echo "weight = ".$weight .'<br>'; 
-echo "pressure = ".$pressure .'<br>'; 
-echo "pulse = ".$pulse .'<br>'; 
-echo "congenOptions = ".$congenOptions .'<br>'; 
-echo "congenInput = ".$congenInput .'<br>'; 
+echo "height = " . $height . '<br>';
+echo "weight = " . $weight . '<br>';
+echo "pressure = " . $pressure . '<br>';
+echo "pulse = " . $pulse . '<br>';
+echo "congenOptions = " . $congenOptions . '<br>';
+echo "congenInput = " . $congenInput . '<br>';
 
-echo "location = ".serialize($location) .'<br>';  
-echo "locationInput = ".$locationInput .'<br>'; 
-echo "period = ".serialize($period) .'<br>';  
-echo "reason1 = ".serialize($reason1) .'<br>';  
-echo "reason1Input = ".$reason1Input .'<br>'; 
-echo "reason2 = ".serialize($reason2) .'<br>';  
-echo "reason2Input = ".$reason2Input .'<br>'; 
-echo "motiOptions = ".$motiOptions .'<br>'; 
-echo "motiInput = ".$motiInput .'<br>'; 
-echo "exer = ".serialize($exer) .'<br>';  
-echo "exerInput = ".$exerInput .'<br>'; 
-echo "pulseAfter = ".$pulseAfter .'<br>'; 
-echo "week = ".$week .'<br>'; 
-echo "intensityOptions = ".$intensityOptions .'<br>'; 
-echo "duration = ".$duration .'<br>'; 
+echo "location = " . serialize($location) . '<br>';
+echo "locationInput = " . $locationInput . '<br>';
+echo "period = " . serialize($period) . '<br>';
+echo "reason1 = " . serialize($reason1) . '<br>';
+echo "reason1Input = " . $reason1Input . '<br>';
+echo "reason2 = " . serialize($reason2) . '<br>';
+echo "reason2Input = " . $reason2Input . '<br>';
+echo "motiOptions = " . $motiOptions . '<br>';
+echo "motiInput = " . $motiInput . '<br>';
+echo "exer = " . serialize($exer) . '<br>';
+echo "exerInput = " . $exerInput . '<br>';
+echo "pulseAfter = " . $pulseAfter . '<br>';
+echo "week = " . $week . '<br>';
+echo "intensityOptions = " . $intensityOptions . '<br>';
+echo "duration = " . $duration . '<br>';
 
 //export object
-$location_data = unserialize(serialize($location)); 
-foreach($location_data  as $loc){
-	echo "location_data = ".$loc .'<br>';  
-}	
-$period_data = unserialize(serialize($period)); 
-foreach($period_data  as $per){
-	echo "period_data = ".$per .'<br>';  
-}	
-$reason1_data = unserialize(serialize($reason1)); 
-foreach($reason1_data  as $rea1){
-	echo "reason1_data = ".$rea1 .'<br>';  
+
+if (!empty($_POST['location'])) {
+	$location_data = unserialize(serialize($location));
+	foreach ($location_data  as $loc) {
+		echo "location_data = " . $loc . '<br>';
+	}
+} else {
+	echo 'location empty' . '<br>';
 }
-$reason2_data = unserialize(serialize($reason2)); 
-foreach($reason2_data  as $rea2){
-	echo "reason2_data = ".$rea2 .'<br>';  
+
+if (!empty($_POST['period'])) {
+	$period_data = unserialize(serialize($period));
+	foreach ($period_data  as $per) {
+		echo "period_data = " . $per . '<br>';
+	}
+} else {
+	echo 'period empty' . '<br>';
 }
-$exer_data = unserialize(serialize($exer)); 
-foreach($exer_data  as $exe){
-	echo "exer_data = ".$exe .'<br>';  
+
+if (!empty($_POST['reason1'])) {
+	$reason1_data = unserialize(serialize($reason1));
+	foreach ($reason1_data  as $rea1) {
+		echo "reason1_data = " . $rea1 . '<br>';
+	}
+} else {
+	echo 'reason1 empty' . '<br>';
+}
+
+if (!empty($_POST['reason2'])) {
+	$reason2_data = unserialize(serialize($reason2));
+	foreach ($reason2_data  as $rea2) {
+		echo "reason2_data = " . $rea2 . '<br>';
+	}
+} else {
+	echo 'reason2 empty' . '<br>';
+}
+
+if (!empty($_POST['exer'])) {
+	$exer_data = unserialize(serialize($exer));
+	foreach ($exer_data  as $exe) {
+		echo "exer_data = " . $exe . '<br>';
+	}
+} else {
+	echo 'exer empty' . '<br>';
 }
 
 //F4
-echo "agency_name1 = ".$agency_name1 .'<br>';
-echo "agency_name2 = ".$agency_name2 .'<br>';
-echo "community = ".$community .'<br>';
-echo "loc_agency = ".$loc_agency .'<br>';
-echo "business = ".$business .'<br>';
+echo "agency_name1 = " . $agency_name1 . '<br>';
+echo "agency_name2 = " . $agency_name2 . '<br>';
+echo "community = " . $community . '<br>';
+echo "loc_agency = " . $loc_agency . '<br>';
+echo "business = " . $business . '<br>';
 
 // F5
-echo "exper_sports = ".$exper_sports .'<br>';
-echo "res = ".$res .'<br>';
-echo "pub_res = ".$pub_res .'<br>';
+echo "exper_sports = " . $exper_sports . '<br>';
+echo "res = " . $res . '<br>';
+echo "pub_res = " . $pub_res . '<br>';
 
 // F6
-echo "train_exper_exer = ".$train_exper_exer .'<br>';
-echo "train_exper = ".$train_exper .'<br>';
+echo "train_exper_exer = " . $train_exper_exer . '<br>';
+echo "train_exper = " . $train_exper . '<br>';
 
 // F7
-echo "vol_exper = ".$vol_exper .'<br>';
+echo "vol_exper = " . $vol_exper . '<br>';
 
 // F8
-echo "org_heal = ".$org_heal .'<br>';
-echo "pro_org_exer = ".$pro_org_exer .'<br>';
-echo "activity = ".$activity .'<br>';
+echo "org_heal = " . $org_heal . '<br>';
+echo "pro_org_exer = " . $pro_org_exer . '<br>';
+echo "activity = " . $activity . '<br>';
+
+//func check null
+function checkOption($option, $checkText, $IP)
+{
+	if ($option == $checkText) {
+		return $IP;
+	} else {
+		return $option;
+	}
+}
+
+function checkEmpty($array, $arrayIP)
+{
+	$countArray = count($array);
+	if ($array == '') {
+		return implode(",", $array);
+	} else {
+		if($countArray = 1){
+			return implode(",", $array);
+		}else{
+			return implode(",", $array) . ',' . $arrayIP;
+		}
+	}
+}
+
+$checkLoc = checkEmpty($location, $locationInput);
+$checkPer = implode(",", $period);
+$checkRes1 = checkEmpty($reason1, $reason1Input);
+$checkRes2 = checkEmpty($reason2, $reason2Input);
+$checkExer = checkEmpty($exer, $exerInput);
+
+$checkEDU = checkOption($eduOptions, 'other', $eduInput);
+$checkOCC = checkOption($occOptions, 'other', $eduInput);
+$checkMary = checkOption($maryOptions, 'other', $maryInput);
+$checkNation = checkOption($nationOptions, 'other', $nationInput);
+$checkCongen = checkOption($congenOptions, 'other', $congenInput);
+$checkMoti = checkOption($motiOptions, 'other', $motiInput);
 
 // เช็คว่ามีข้อมูลนี้อยู่หรือไม่
 $check = "select * from question  where username = '$user' ";
-$result1 = mysqli_query($con,$check) or die("$check");
-  $num=mysqli_num_rows($result1); 
-  if($num > 0)   		
-  {
-//ถ้ามี username นี้อยู่ในระบบแล้วให้แจ้งเตือน
-	   echo "<script>";
-	   echo "alert(' username นี้ทำแบบสอบถามแล้ว  !');";
-	   echo "window.location='page.php';";
-		 echo "</script>";
+$result1 = mysqli_query($con, $check) or die("$check");
+$num = mysqli_num_rows($result1);
+if ($num > 0) {
+	//ถ้ามี username นี้อยู่ในระบบแล้วให้แจ้งเตือน
+	echo "<script>";
+	echo "alert(' username นี้ทำแบบสอบถามแล้ว  !');";
+	echo "window.location='page.php';";
+	echo "</script>";
+} else {
+	//เพิ่มเข้าไปในฐานข้อมูล
+	$sql = "INSERT INTO question(username,name,surname,sex,province_id,amphure_id,age,height,weight,pressure,
+								 pulse,location,period,reason1,reason2,exer,pulseAfter,week,duration,agency_name1,agency_name2,
+								 community,loc_agency,business,exper_sports,res,pub_res,train_exper_exer,train_exper,vol_exper,
+								 org_heal,pro_org_exer,activity,eduOptions,occOptions,maryOptions,nationOptions,congenOptions,
+								 motiOptions,intensityOptions)
+			 VALUES('$user','$name', '$surname','$sex','$province_id','$amphure_id','$age','$height','$weight','$pressure',
+								 '$pulse','$checkLoc','$checkPer','$checkRes1','$checkRes2','$checkExer','$pulseAfter','$week','$duration','$agency_name1','$agency_name2',
+								 '$community','$loc_agency','$business','$exper_sports','$res','$pub_res','$train_exper_exer','$train_exper','$vol_exper',
+								 '$org_heal','$pro_org_exer','$activity','$checkEDU','$checkOCC','$checkMary','$checkNation','$checkCongen',
+								 '$checkMoti','$checkIntensity')";
 
-  }else{
-//เพิ่มเข้าไปในฐานข้อมูล
-$sql = "INSERT INTO question(username,name,surname,)
-			 VALUES('$user','$name', '$surname')";
-
-$result = mysqli_query($con, $sql) or die("Error in query: $sql " );
-  }
+	$result = mysqli_query($con, $sql) or die("Error in query: $sql ");
+}
 //ปิดการเชื่อมต่อ database
 mysqli_close($con);
 //จาวาสคริปแสดงข้อความเมื่อบันทึกเสร็จและกระโดดกลับไปหน้าฟอร์ม
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <?php
     include 'bootstrap.php';
-    include 'condb.php' ;
+    include 'condb.php';
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,9 +13,39 @@
 
 <body style="background: linear-gradient(90deg, #C7C5F4, #776BCC);	">
     <div class="mt-5 h-75 d-flex align-items-center justify-content-center">
-        <div class="" style="width: 60rem;">
+        <div class="">
             <div class="card-body">
                 <center>
+                    <div class="card" style="width: 25rem;">
+                        <!-- <img src="assets/img/apple-touch-icon.png" class="card-img-top" alt="img"> -->
+                        <div class="card-body">
+                            <h5 class="card-title">Welcome</h5>
+                            <p class="card-text">Who Are You ?</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <button type="button" class="zzz btn btn-light list-group-item" id="Interested-Individual" data-bs-toggle="modal" data-bs-target="#Modal">
+                                <h5>Interested-Individual</h5>
+                            </button>
+                            <button type="button" class="zzz btn btn-light list-group-item" id="Trainers" data-bs-toggle="modal" data-bs-target="#Modal">
+                                <h5>Trainers</h5>
+                            </button>
+                            <button type="button" class="zzz btn btn-light list-group-item" id="Sport-professionals" data-bs-toggle="modal" data-bs-target="#Modal">
+                                <h5>Sport-professionals</h5>
+                            </button>
+                            <button type="button" class="zzz btn btn-light list-group-item" id="Volunteer" data-bs-toggle="modal" data-bs-target="#Modal">
+                                <h5>Volunteer</h5>
+                            </button>
+                            <button type="button" class="zzz btn btn-light list-group-item" id="Personnel-Support-Staff" data-bs-toggle="modal" data-bs-target="#Modal">
+                                <h5>Personnel/Support-Staff</h5>
+                            </button>
+                            <button type="button" class="zzz btn btn-light list-group-item" id="Suppliers/Partners" data-bs-toggle="modal" data-bs-target="#Modal">
+                                <h5>Suppliers/Partners</h5>
+                            </button>
+                            <button type="button" class="zzz btn btn-light list-group-item" id="Community" data-bs-toggle="modal" data-bs-target="#Modal">
+                                <h5>Community</h5>
+                            </button>
+                        </ul>
+                    </div>
                     <?php include "tapSelect.php" ?>
                 </center>
             </div>
