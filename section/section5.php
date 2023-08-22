437f1f85ce83a56c0a80673071c0906da99bf3e0
001 <001@mail>
2023-08-22 23:02:19 +0700

    fix

--- section/section5.php
+++ section/section5.php
@@ -14,12 +14,25 @@
 
         <div class=" mb-3">
             <p>สาขาความเชี่ยวชาญทางด้านวิทยาศาสตร์การกีฬาเพื่อสุขภาพของผู้เชี่ยวชาญทั้งหมด</p>
-            <input name="res" type="text" class="form-control" value="" disabled>
+            <input name="exper_sports" type="text" class="form-control" value="" disabled>
         </div>
 
         <div class=" mb-3">
-            <p>สาขาความเชี่ยวชาญทางด้านวิทยาศาสตร์การกีฬาเพื่อสุขภาพของผู้เชี่ยวชาญทั้งหมด แยกตามจังหวัด</p>
-            <input name="res" type="text" class="form-control" value="" disabled>
+            <div class="form-row">
+                <div class="form-group">
+                    <p> สาขาความเชี่ยวชาญทางด้านวิทยาศาสตร์การกีฬาเพื่อสุขภาพของผู้เชี่ยวชาญทั้งหมด แยกตามจังหวัด</p>
+                    <select name="province_id" id="provinceEXsportS5" class="form-control mb-3" required>
+                        <option value="<?php echo $fetch['province_id']; ?>">เลือกจังหวัด</option>
+                        <?php
+                        $sqlProvinceEXsportS5 = "SELECT * FROM provinces";
+                        $queryProvinceEXsportS5 = mysqli_query($con, $sqlProvinceEXsportS5);
+                        while ($resultProvinceEXsportS5 = mysqli_fetch_assoc($queryProvinceEXsportS5)) : ?>
+                            <option value="<?= $resultProvinceEXsportS5['id'] ?>"><?= $resultProvinceEXsportS5['name_th'] ?></option>
+                        <?php endwhile; ?>
+                    </select>
+                </div>
+            </div>
+            <div id="exSportS5" class="form-control"></div>
         </div>
 
         <div class=" mb-3">
@@ -37,7 +50,7 @@
                 <div class="form-group">
                     <p>งานวิจัยทั้งหมดแยกจังหวัด</p>
                     <select name="province_id" id="provinceResS5" class="form-control mb-3" required>
-                        <option value="">เลือกจังหวัด</option>
+                        <option value="<?php echo $fetch['province_id']; ?>">เลือกจังหวัด</option>
                         <?php
                         $sqlProvinResS5 = "SELECT * FROM provinces";
                         $queryProvinResS5  = mysqli_query($con, $sqlProvinResS5);
@@ -65,7 +78,7 @@
                 <div class="form-group">
                     <p> การเผยแพร่ผลงานวิจัยรายจังหวัด</p>
                     <select name="province_id" id="provincePubresS5" class="form-control mb-3" required>
-                        <option value="">เลือกจังหวัด</option>
+                        <option value="<?php echo $fetch['province_id']; ?>">เลือกจังหวัด</option>
                         <?php
                         $sqlProvinPubresS5 = "SELECT * FROM provinces";
                         $queryProvinPubresS5 = mysqli_query($con, $sqlProvinPubresS5);
@@ -83,84 +96,137 @@
 
 <script>
     $(function() {
-        var provinceObject = $('#provinceResS5');
-        var pubResObject = $('#pubresS5');
+        var provinceExSportObject = $('#provinceEXsportS5');
+        var exSportObject = $('#exSportS5');
 
-        // on change province for research results
-        provinceObject.on('change', function() {
+        exSportObject.hide();
+
+        provinceExSportObject.on('change', function() {
+            exSportObject.show();
             var provinceId = $(this).val();
+            exSportObject.empty();
 
-            // Clear existing content in pubResObject
-            pubResObject.empty();
-
-            var url = 'get_Que_Provin.php?province_id=%27' + provinceId + '%27';
+            var url = 'get_Que_Provin.php?province_id=' + provinceId;
 
             $.get(url, function(data) {
                 var result = JSON.parse(data);
                 var itemCount = 0;
+                var resArray = [];
 
-                // Count the number of items and show each valid item in the result
                 $.each(result, function(index, item) {
-                    if (item.pub_res !== "---,---" && item.pub_res.trim() !== "") {
+                    if (item.exper_sports !== "---,---" && item.exper_sports.trim() !== "") {
                         itemCount++;
+                        var resText = item.exper_sports;
 
-                        // Check for comma and add line break
-                        var pubResText = item.pub_res;
-                        if (pubResText.includes(',')) {
-                            pubResText = pubResText.replace(/,/g, '<br>');
+                        if (resText.includes(',')) {
+                            var resItems = resText.split(',');
+                            resArray = resArray.concat(resItems);
+                        } else {
+                            resArray.push(resText);
                         }
-
-                        pubResObject.append($('<div></div>').html('ได้แก่ ' + pubResText));
                     }
                 });
 
-                // Show the number of valid items found
-                pubResObject.append($('<div></div>').text('พบ = ' + itemCount + ' รายการ '));
+                $.each(resArray, function(index, resItem) {
+                    exSportObject.append($('<div></div>').html('สาขาความเชี่ยวชาญ : ' + resItem));
+                });
+
+                exSportObject.append($('<div></div>').text('พบ = ' + resArray.length + ' รายการ ')); 
             }).fail(function() {
-                // Handle error case here
-                pubResObject.empty();
-                pubResObject.append($('<div></div>').text('เกิดข้อผิดพลาดในการดึงข้อมูล'));
+                exSportObject.empty();
+                exSportObject.append($('<div></div>').text('เกิดข้อผิดพลาดในการดึงข้อมูล'));
             });
         });
+
+
+
+        var provinceObject = $('#provinceResS5');
+        var ResObject = $('#resS5');
+
+        ResObject.hide();
+
+        provinceObject.on('change', function() {
+            ResObject.show();
+            var provinceId = $(this).val();
+            ResObject.empty();
+
+            var url = 'get_Que_Provin.php?province_id=' + provinceId;
+
+            $.get(url, function(data) {
+                var result = JSON.parse(data);
+                var itemCount = 0;
+                var resArray = []; // เพิ่มตัวแปรเพื่อเก็บค่า item.pub_res ที่มี comma
+
+                $.each(result, function(index, item) {
+                    if (item.res !== "---,---" && item.res.trim() !== "") {
+                        itemCount++;
+                        var resText = item.res;
+
+                        if (resText.includes(',')) {
+                            var resItems = resText.split(','); // แยกข้อมูลด้วย comma เพื่อสร้าง Array
+                            resArray = resArray.concat(resItems); // เพิ่มข้อมูลใน Array
+                        } else {
+                            resArray.push(resText);
+                        }
+                    }
+                });
+
+                // วนลูปเพื่อแทรกข้อมูลใน Array เข้าใน ResObject
+                $.each(resArray, function(index, resItem) {
+                    ResObject.append($('<div></div>').html('งานวิจัย : ' + resItem));
+                });
+
+                ResObject.append($('<div></div>').text('พบ = ' + (resArray.length) + ' รายการ '));
+            }).fail(function() {
+                ResObject.empty();
+                ResObject.append($('<div></div>').text('เกิดข้อผิดพลาดในการดึงข้อมูล'));
+            });
+        });
+
 
         // on change province for publication results
         var provincePubresObject = $('#provincePubresS5');
         var pubResObject = $('#pubresS5');
 
+        pubResObject.hide();
+
         provincePubresObject.on('change', function() {
             var provinceId = $(this).val();
-
-            // Clear existing content in pubResObject
             pubResObject.empty();
-            
-            var url = 'get_Que_Provin.php?province_id=%27' + provinceId + '%27';
+
+            pubResObject.show();
+
+            var url = 'get_Que_Provin.php?province_id=' + provinceId;
 
             $.get(url, function(data) {
                 var result = JSON.parse(data);
                 var itemCount = 0;
+                var resArray = [];
 
-                // Count the number of items and show each valid item in the result
                 $.each(result, function(index, item) {
-                    if (item.pub_res !== "---,---" && item.pub_res.trim() !== "") {
+                    if (item.res !== "---,---" && item.res.trim() !== "") {
                         itemCount++;
+                        var resText = item.res;
 
-                        // Check for comma and add line break
-                        var pubResText = item.pub_res;
-                        if (pubResText.includes(',')) {
-                            pubResText = pubResText.replace(/,/g, '<br>');
+                        if (resText.includes(',')) {
+                            var resItems = resText.split(',');
+                            resArray = resArray.concat(resItems);
+                        } else {
+                            resArray.push(resText);
                         }
-
-                        pubResObject.append($('<div></div>').html('ได้แก่ ' + pubResText));
                     }
                 });
 
-                // Show the number of valid items found
-                pubResObject.append($('<div></div>').text('พบ = ' + itemCount + ' รายการ '));
+                $.each(resArray, function(index, resItem) {
+                    pubResObject.append($('<div></div>').html('การเผยแพร่ผลงานวิจัย : ' + resItem));
+                });
+
+                pubResObject.append($('<div></div>').text('พบ = ' + resArray.length + ' รายการ '));
             }).fail(function() {
-                // Handle error case here
                 pubResObject.empty();
                 pubResObject.append($('<div></div>').text('เกิดข้อผิดพลาดในการดึงข้อมูล'));
             });
         });
+
     });
 </script>
