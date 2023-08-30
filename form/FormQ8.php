426b4e9a695600a745689fc43ef0ffca580ed43f
001 <001@mail>
2023-08-30 13:24:29 +0700

    fix

--- form/FormQ8.php
+++ form/FormQ8.php
@@ -3,27 +3,34 @@
 <!-- org_heal form -->
 <div class="form-control mb-3">
     <p>ประสบการณ์การจัดกิจกรรมทางด้านการออกกำลังกายเพื่อสุขภาพ</p>
-    <div class="form-check">
-        <input class="form-check-input" type="radio" name="org_heal" value="มี" id="org_heal" <?php if ($foundUser == 1 && $org_heal == "มี") echo "checked"; ?>>
-        <label class="form-check-label" for="org_heal">
-            มี
-        </label>
-    </div>
-    <div class="form-check">
-        <input class="form-check-input" type="radio" name="org_heal" value="ไม่มี" id="org_heal" <?php if ($foundUser == 1 && $org_heal == "ไม่มี") echo "checked"; ?>>
-        <label class="form-check-label" for="org_heal">
-            ไม่มี
-        </label>
-    </div>
-    <div class="alert alert-danger mb-3" id="emptyAlert-org_heal" style="display: none;">
-        กรุณาเลือกข้อมูล
-    </div>
+    <?php if ($_SESSION["level"] == 'Personnel/Support-Staff') { ?>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="org_heal" value="มี" id="org_heal" <?php if ($foundUser == 1 && $org_heal == "มี") echo "checked"; ?>>
+            <label class="form-check-label" for="org_heal">
+                มี
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="org_heal" value="ไม่มี" id="org_heal" <?php if ($foundUser == 1 && $org_heal == "ไม่มี") echo "checked"; ?>>
+            <label class="form-check-label" for="org_heal">
+                ไม่มี
+            </label>
+        </div>
+        <div class="alert alert-danger mb-3" id="emptyAlert-org_heal" style="display: none;">
+            กรุณาเลือกข้อมูล
+        </div>
+    <?php } else { ?>
+        <input class="form-control mb-3" type="text" name="org_heal" value="<?php echo $foundUser == 1 ? $exper_sports : ''; ?>" id="orgHeal">
+        <div class="alert alert-danger mb-3" id="emptyAlert-orgHeal" style="display: none;">
+            กรุณากรอกประสบการณ์การจัดกิจกรรมทางด้านการออกกำลังกายเพื่อสุขภาพ
+        </div>
+    <?php } ?>
 </div>
 <!-- end org_heal form -->
 
-<?php  
+<?php
 $defaultValueProOrgExer = $foundUser == 1 ? $pro_org_exer : '';
-$defaultValueActivityName = $foundUser == 1 ? $activity : ''; 
+$defaultValueActivityName = $foundUser == 1 ? $activity : '';
 echo generateFormField("proOrgExer", "โครงการการจัดกิจกรรมการออกกำลังกายเพื่อสุขภาพ", "", true, true, $defaultValueProOrgExer);
 echo generateFormField("activityName", "กิจกรรม/โครงการที่สนับสนุนการจัดกิจกรรม", "", true, true, $defaultValueActivityName);
 ?>
@@ -32,12 +39,12 @@ echo generateFormField("activityName", "กิจกรรม/โครงกา
     $(document).ready(function() {
         const radioName = "org_heal";
         const formIds = [
-            "proOrgExer", "activityName"
+            "proOrgExer", "activityName", "orgHeal"
         ];
 
         $(`input[name=${radioName}]`).on("change", checkAndUpdate);
 
-        formIds.forEach(id => $(`[name="${id}"]`).on("input change", checkAndUpdate));
+        formIds.forEach(id => $(`[id="${id}"]`).on("input change", checkAndUpdate));
 
         function checkAndUpdate() {
             checkRadio();
